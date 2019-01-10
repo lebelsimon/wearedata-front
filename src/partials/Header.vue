721dc21514bae1fb18a1f1a6fd212bd523989a08
frednad <corbeille.i@laposte.net>
2019-01-10 18:16:32 +0100

    add compass & files

---
+++ src/partials/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <div class="heading">
+            <p>votre plateforme de reporting et dataviz</p>
+            <img src="../assets/LOGO-BIG.svg" alt="logo">
+            <p>démocratisez l'accès à vos données d'entreprise</p>
+            <button>soyez les premiers !</button>
+
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .heading {
+        display: block;
+        background-image: url("../assets/bg-header.png");
+
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,7 @@
 
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    
-    
-    <h1>Je suis le dev' le plus chaud du monde</h1>
+  
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
