078032da44b0d10609ec776b45355e9cb52a501a
frednad <corbeille.i@laposte.net>
2019-01-10 15:10:33 +0100

    Add scss preprocessing method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,21 @@
 <template>
+
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    
+    
+    <h1>Je suis le dev' le plus chaud du monde</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+
+
+<style lang="scss" scoped>
+    h1{
+      color: blue;
+  }
+</style>
+
 
 <script>
 // @ is an alias to /src
