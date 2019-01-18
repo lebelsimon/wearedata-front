0d156b3b36930c0767645c4559809cc3dab78d46
frednad <corbeille.i@laposte.net>
2019-01-18 12:10:04 +0100

    Update front files

--- src/partials/Header.vue
+++ src/partials/Header.vue
@@ -1,20 +1,72 @@
 <template>
     <header>
-        <div class="header">
+        <section class="header">
             <section class="hero-home">
                 <div class="hero-overlay">
-                    <p id="prem">votre plateforme de reporting et dataviz</p>
-                    <img src="../assets/LOGO-BIG.svg" alt="logo">
-                    <div id ="deux">
-                    <p class="uppercase">démocratisez l'accès à vos données d'entreprise </p>
-                    <div class="arrounded">soyez les premiers !</div>
+                    <p class="firstPart">votre plateforme de reporting et dataviz</p>
+                   <img src="../assets/image/LOGO-BIG.svg" alt="logo">
+                    <div class ="secondPart">
+                        <p class="uppercase">démocratisez l'accès à vos données d'entreprise </p>
+                        <div class="btn btn-blue btn-rounded">soyez les premiers !</div>
                     </div>
-                    <div class="uppercase">
-                        explore
+                    <div class="tirdPart">
+                        <div id="explore" class="uppercase arrounded">
+                            explore
+                        </div>
                     </div>
                 </div>
             </section>
-        </div>
+            
+        </section>
+        <section>
+                <div class="second-section">
+                    <div class="here">
+                        Voici WeAreData 
+                    </div>
+                    <div class="application uppercase">
+                        Application de dataviz 
+                        financière pour les PME
+                    </div>
+                    
+                    <div class="lorem">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                    <section class="flex">
+                        <div class="wrapper">
+                           <img src="../assets/image/icon4.svg" alt="reporting-cealises">
+                           <p class="inner-text">Vos reporting centralisés</p> 
+                        </div>
+                        <div class="wrapper">
+                           <img src="../assets/image/icon3.svg" alt="suivi-de-vostivites">
+                           <p class="inner-text">Le suivi de vos activités en temps réel</p> 
+                        </div>
+                        <div class="wrapper">
+                           <img src="../assets/image/icon2.svg" alt="pilotage">
+                           <p class="inner-text">Un gain de temps pour le pilotage</p> 
+                        </div>
+                        <div class="wrapper">
+                           <img src="../assets/image/icon1.svg" alt="entreprise">
+                            <p class="inner-text"> Des  utilitaires pour votre entreprise</p> 
+                        </div>
+                    </section>
+                        <div class="first-section">
+                            <div class="title uppercase">
+                                comment ça marche ?
+                            </div>
+                            <div>
+                                Vos reporting d’entreprise
+                                réuni sur une 
+                                seule application
+                            </div>
+                            <div>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </div>
+                        </div>
+                    <section>
+
+                    </section>
+                </div>
+            </section>
     </header>
 </template>
 
@@ -25,61 +77,7 @@
 </script>
 
 <style lang="scss">
-    .uppercase{
-        text-transform: uppercase;
-    }
-    .heading {
-        background-color: rgba(96,96,96,0.7);
-    }
-    .hero-home{
-    height: 500px;
-    background-image: url("../assets/bg-header.png");
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    position: relative;
-    .hero-overlay{  
-        width: 100%;
-        height: 100%;
-        background-color: rgba(96,96,96,0.7); 
-    }
-    #prem{
-        position: absolute;
-        top: 20%;
-        left: 50%;
-        color: white;
-        transform: translate(-50%,-50%);
-        padding:0px;
-        font-size: 24px;
-        letter-spacing: 3px;
-    }
-    img{
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        color: white;
-        transform: translate(-50%,-50%);
-        padding:0px;
-        font-size: 40px;
-        letter-spacing: 3px;
-        font-weight: 800;
-        // animation: 2s textEffect alternate infinite;
-    }
-
-    #deux{
-        position: absolute;
-        top: 70%;
-        left: 50%;
-        color: white;
-        transform: translate(-50%,-50%);
-        padding:0px;
-        font-size: 20px;
-        letter-spacing: 3px;
-        font-weight: 800;
-        // animation: 2s textEffect alternate infinite;
-    }
-
-}
+   
 
 </style>
 
