36b02379f07261be92ceaa2301d86869273d47a6
fred <frederic.nadaradjane@edu.univ-paris13.fr>
2019-01-24 18:04:57 +0100

    Footer 95% done

--- src/partials/Footer.vue
+++ src/partials/Footer.vue
@@ -3,6 +3,22 @@
         <div class="logo-footer">
             <img class="logo" src="../assets/image/LOGO-BIG.svg" alt="logo-footer">
         </div>
+       
         <p class="copyright">copyright 2019</p>
+
+         <div class = "social_network">
+             <div class="social_item">
+                 <i class="fab fa-facebook-f elmt"></i>
+             </div>
+             
+             <div class="social_item">
+                 <i class="fab fa-twitter elmt"></i>
+             </div>
+
+             <div class="social_item">
+                 <i class="fab fa-linkedin-in elmt"></i>
+             </div>
+
+         </div>
     </footer>
 </template>
