2066092f1858c5688f1937ad74bed108eac21168
frednad <corbeille.i@laposte.net>
2019-01-25 10:40:24 +0100

    Add connection and subscribe buttons, add fontawesome icons

--- src/partials/Header.vue
+++ src/partials/Header.vue
@@ -1,69 +1,21 @@
 <template>
-    <header>
-        <section class="header">
-            <section class="hero-home">
-                <div class="hero-overlay">
-                    <p class="firstPart">votre plateforme de reporting et dataviz</p>
-                   <img src="../assets/image/LOGO-BIG.svg" alt="logo">
-                    <div class ="secondPart">
-                        <p class="uppercase">démocratisez l'accès à vos données d'entreprise </p>
-                        <div class="btn btn-blue btn-rounded">soyez les premiers !</div>
-                    </div>
-                    <div class="tirdPart">
-                        <div id="explore" class="uppercase arrounded">
-                            explore
-                        </div>
-                    </div>
-                </div>
-            </section>
-            
-        </section>
-        <section>
-                <div class="second-section">
-                    <div class="here">
-                        Voici WeAreData 
-                    </div>
-                    <div class="application uppercase">
-                        Application de dataviz 
-                        financière pour les PME
-                    </div>
-                    
-                    <div class="lorem">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </div>
-                    <section class="flex">
-                        <div class="wrapper">
-                           <img src="../assets/image/icon4.svg" alt="reporting-cealises">
-                           <p class="inner-text">Vos reporting centralisés</p> 
-                        </div>
-                        <div class="wrapper">
-                           <img src="../assets/image/icon3.svg" alt="suivi-de-vostivites">
-                           <p class="inner-text">Le suivi de vos activités en temps réel</p> 
-                        </div>
-                        <div class="wrapper">
-                           <img src="../assets/image/icon2.svg" alt="pilotage">
-                           <p class="inner-text">Un gain de temps pour le pilotage</p> 
-                        </div>
-                        <div class="wrapper">
-                           <img src="../assets/image/icon1.svg" alt="entreprise">
-                            <p class="inner-text"> Des  utilitaires pour votre entreprise</p> 
-                        </div>
-                    </section>
-
-                </div>
-            </section>
-    </header>
+   
+<ul class="navigation">
+  <li><a href="#">Connection</a></li>
+  <li><a href="#">Inscription</a></li>
+</ul>
 
 </template>
 
 <script>
-    export default {
 
-    }
+export default {
+    
+}
 </script>
 
 <style lang="scss">
-   
+
 
 </style>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,76 @@
 <template>
     <div class="home">
         <section>
+        <section class="head">
+            <section class="hero-home">
+                <div class="hero-overlay">
+                    <p class="firstPart">votre plateforme de reporting et dataviz</p>
+                   <img src="../assets/image/LOGO-BIG.svg" alt="logo">
+                    <div class ="secondPart">
+                        <p class="uppercase">démocratisez l'accès à vos données d'entreprise </p>
+                        <div class="btn btn-blue btn-rounded">soyez les premiers !</div>
+                    </div>
+
+                    <div class = "social_network_head">
+                            <div class="social_item">
+                                <i class="fab fa-facebook-f elmt"></i>
+                            </div>
+                            
+                            <div class="social_item">
+                                <i class="fab fa-twitter elmt"></i>
+                            </div>
+
+                            <div class="social_item">
+                                <i class="fab fa-linkedin-in elmt"></i>
+                            </div>
+                    </div>
+                    <div id="explore" class="uppercase arrounded">
+                            explore <i class="fas fa-angle-down"></i>
+                    </div>
+                    <div class="tirdPart">
+            
+                       
+                    </div>
+                </div>
+            </section>
+            
+        </section>
+        <section>
+                <div class="second-section">
+                    <div class="here">
+                        Voici WeAreData 
+                    </div>
+                    <div class="application uppercase">
+                        Application de dataviz 
+                        financière pour les PME
+                    </div>
+                    
+                    <div class="lorem">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                    <section class="flex">
+                        <div class="wrapper">
+                           <img src="../assets/image/icon4.svg" alt="reporting-cealises">
+                           <p class="inner-text">Vos reporting centralisés</p> 
+                        </div>
+                        <div class="wrapper">
+                           <img src="../assets/image/icon3.svg" alt="suivi-de-vostivites">
+                           <p class="inner-text">Le suivi de vos activités en temps réel</p> 
+                        </div>
+                        <div class="wrapper">
+                           <img src="../assets/image/icon2.svg" alt="pilotage">
+                           <p class="inner-text">Un gain de temps pour le pilotage</p> 
+                        </div>
+                        <div class="wrapper">
+                           <img src="../assets/image/icon1.svg" alt="entreprise">
+                            <p class="inner-text"> Des  utilitaires pour votre entreprise</p> 
+                        </div>
+                    </section>
+
+                </div>
+            </section>
+    </section>
+        <section>
             <div class="first-section">
                 <div class="first-elem">
                     <div class="title uppercase">comment ça marche ?</div>
