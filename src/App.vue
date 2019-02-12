c0eef2c0d9fa2dab3b5dd106931aae7019b7e93e
youu9 <chbada.younes@gmail.com>
2019-02-12 19:50:23 +0100

    Add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
-    <Header />
-    <router-view />
-    <Footer />
-  </div>
+    <!-- <div class="container"> -->
+      <Header />
+        <router-view />
+      <Footer />
+    </div>
+  <!-- </div> -->
 
 </template>
 
@@ -21,23 +23,10 @@
 
 <style lang="scss">
 @import "./assets/css/style.css";
-  #app {
-    -webkit-font-smoothing: antialiased;
-    //-moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-
-  // #nav {
-  //   padding: 30px;
-
-  //   a {
-  //     font-weight: bold;
-  //     color: #2c3e50;
-
-  //     &.router-link-exact-active {
-  //       color: #42b983;
-  //     }
-  //   }
+  // #app {
+  //   -webkit-font-smoothing: antialiased;
+  //   //-moz-osx-font-smoothing: grayscale;
+  //   text-align: center;
+  //   color: #2c3e50;
   // }
 </style>
