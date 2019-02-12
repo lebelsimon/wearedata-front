19141e948d85b59033628726687e5bbbfd325dd7
youu9 <chbada.younes@gmail.com>
2019-02-12 19:57:52 +0100

    add redirection to home on footer logo

--- src/partials/Footer.vue
+++ src/partials/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <div class="logo-footer">
-            <img class="logo" src="../assets/image/LOGO-BIG.svg" alt="logo-footer">
+            <router-link to="/"><img class="logo" src="../assets/image/LOGO-BIG.svg" alt="logo-footer"></router-link>
         </div>
        
         <p class="copyright">copyright 2019</p>
