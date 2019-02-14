e2908f858968a043614f0e705a725c2ff02e9566
youu9 <chbada.younes@gmail.com>
2019-02-14 12:15:19 +0100

    add bills

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Dashboard from './views/loggedviews/Dashboard.vue';
+import Bills from './views/loggedviews/Bills.vue';
 import Logout from './components/basics/login/Logout.vue'
 
 Vue.use(Router);
@@ -13,6 +14,7 @@ export default new Router({
     {path: '/login',name: 'login', component: () => import('./components/basics/login/Login.vue')},
     {path: '/signup',name: 'signup', component: () => import('./components/basics/register/SignUp.vue')},
     {path: '/dashboard',name: 'dashboard',component: Dashboard},
+    {path: '/bills',name: 'bills',component: Bills},
     {path: '/logout',name: 'Logout',component: Logout}
   ]
 });
